7f481509682ba1c99df51792e27cd7ca265379f8
SohamJain789 <69146549+SohamJain789@users.noreply.github.com>
2020-08-22 22:52:06 +0530

    Add files via upload

--- Ball.js
+++ Ball.js
@@ -3,26 +3,29 @@ constructor(x,y,radius){
     var options = {
     
     isStatic:false,
-    //restitution:0.3
+    restitution:0.3,
     friction:0.5,
     density:1.2
 }
-this.body = Matter.Bodies.circle(x,y,radius,options);
+this.x=x;
+this.y=y;
 this.radius = radius;
+this.body = Matter.Bodies.circle(this.x,this.y,this.radius/2,options);
+
 
 World.add(world,this.body);
 
 }
 display(){
 var pos =this.body.position;
-var angle = this.body.angle;
+
 push();
 translate(pos.x,pos.y);
-fill("green");
-ellipse(pos.x,pos.y,width,height);
 
+fill("white");
+ellipseMode(RADIUS);
+
+ellipse(0,0,this.r,this.r);
+pop();
 } 
 }
-
-
-
--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -7,24 +6,25 @@ var paperBall,ground,dustbin;
 
 function preload()
 {
-	
+  
 }
 
 function setup() {
-	//createCanvas(800, 700);
+  //createCanvas(800, 700);
   createCanvas(1600, 700);
-	rectMode(CENTER);
+  rectMode(CENTER);
 
-	engine = Engine.create();
-	world = engine.world;
+  engine = Engine.create();
+  world = engine.world;
 
-	//Create the Bodies Here.
-    paperBall = new Ball (50,580,20);
+  //Create the Bodies Here.
+    //paperBall = new Ball (50,580,20);
+    paperBall = new Ball(200,450,100);
     //ground = new Ground (350,790,700,20);
     ground = new Ground(width/2,670,width,20)
     dustbin=new Dustbin(1200,650);
     //dustbin = new Dustbin;
-	Engine.run(engine);
+  Engine.run(engine);
   
 }
 
@@ -35,9 +35,17 @@ function draw() {
   ground.display();
   dustbin.display();
   paperBall.display();
+  keyPressed()
   //drawSprites();
  
 }
 
+function keyPressed(){
+if (keyCode === UP_ARROW){
+
+Matter.Body.applyForce(paperBall.body,paperBall.body.position,{x:5,y:-25});
+
+}
 
 
+}
